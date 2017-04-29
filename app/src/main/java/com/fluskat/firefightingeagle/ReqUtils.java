ee0bb2563e995fba3ba946af285cf3df667ce4cf
agoncecelia <agonn.c@gmail.com>
2017-04-30 01:51:53 +0200

    initial commit

---
+++ app/src/main/java/com/fluskat/firefightingeagle/ReqUtils.java
@@ -0,0 +1,68 @@
+package com.fluskat.firefightingeagle;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * Created by Erenis on 05-Feb-16.
+ */
+public class ReqUtils {
+    private static final String TAG = ReqUtils.class.getSimpleName();
+
+    public static void jsonRequestWithHeaders(Context act, int reqType, String url, final Map<String, String> header,
+                                              Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        JsonObjectRequest request = new JsonObjectRequest(reqType, url, null, listener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return header;
+            }
+        };
+        MySingleton.getInstance(act).addToRequestQueue(request);
+    }
+
+    public static void stringRequestWithHeaders(Activity act, int reqType, String url, Response.Listener<String> listener,
+                                                Response.ErrorListener errorListener, final Map<String, String> headers) {
+        StringRequest request = new StringRequest(reqType, url, listener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return headers;
+            }
+        };
+        MySingleton.getInstance(act).addToRequestQueue(request);
+    }
+
+    public static void jsonRequestWithParams(Activity act, int reqType, String url, JSONObject params,
+                                             Response.Listener<JSONObject> listener,
+                                             Response.ErrorListener errorListener) {
+        JsonObjectRequest request = new JsonObjectRequest(reqType, url, params, listener, errorListener);
+        MySingleton.getInstance(act).addToRequestQueue(request);
+    }
+
+    public static void jsonRequestWithHeadersAndParams(Activity act, int reqType, String url,
+                                                       final Map<String, String> headers, JSONObject params,
+                                                       Response.Listener<JSONObject> listener,
+                                                       Response.ErrorListener errorListener) {
+        JsonObjectRequest request = new JsonObjectRequest(reqType, url, params, listener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return headers;
+            }
+        };
+        try {
+            Log.d("TAG12", "Request token: " + request.getHeaders().get("Authorization"));
+        } catch (AuthFailureError authFailureError) {
+            authFailureError.printStackTrace();
+        }
+        MySingleton.getInstance(act).addToRequestQueue(request);
+    }
+}
---
+++ app/src/main/java/com/fluskat/firefightingeagle/User.java
@@ -0,0 +1,44 @@
+package com.fluskat.firefightingeagle;
+
+/**
+ * Created by Dell on 4/29/2017.
+ */
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String gcmToken;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGcmToken() {
+        return gcmToken;
+    }
+
+    public void setGcmToken(String gcmToken) {
+        this.gcmToken = gcmToken;
+    }
+}
