8728122e232f8dcedff215c53c8864c1d82f01c2
erenis_ramadani <erenisramadani@gmail.com>
2017-04-30 11:19:34 +0200

    Login done

--- app/src/main/java/com/fluskat/firefightingeagle/RegisterActivity.java
+++ app/src/main/java/com/fluskat/firefightingeagle/RegisterActivity.java
@@ -2,14 +2,12 @@ package com.fluskat.firefightingeagle;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -19,51 +17,69 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by Erenis Ramadani on 30-Apr-17. Docs
+ * Created by Erenis Ramadani on 30-Apr-17.
  */
 
-public class LoginActivity extends AppCompatActivity
+public class RegisterActivity extends AppCompatActivity
 {
-    private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+
+    private EditText mName;
 
     private EditText mEmail;
 
+    private EditText mUserName;
+
     private EditText mPassword;
+
+    private EditText mRepeatPassword;
+
+    private Button mRegister;
 
     private String email;
 
     private String password;
 
-    private LoginActivity getContext()
+    private String userName;
+
+    private String name;
+
+    private RegisterActivity getContext()
     {
-        return LoginActivity.this;
+        return RegisterActivity.this;
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
+
         initViews();
     }
 
-    //------------------------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------
 
     private void initViews()
     {
-        mEmail = (EditText) findViewById(R.id.widget_edit_text_email_login);
-        mPassword = (EditText) findViewById(R.id.widget_edit_text_password_login);
-        Button mLogin = (Button) findViewById(R.id.widget_button_login);
-        TextView noAccount = (TextView) findViewById(R.id.widget_text_view_no_account_login);
-        noAccount.setOnClickListener(mOnClickListener);
-        mLogin.setOnClickListener(mOnClickListener);
+        mName = (EditText) findViewById(R.id.widget_edit_text_name_register);
+        mEmail = (EditText) findViewById(R.id.widget_edit_text_email_register);
+        mUserName = (EditText) findViewById(R.id.widget_edit_text_username_register);
+        mPassword = (EditText) findViewById(R.id.widget_edit_text_password_register);
+        mRepeatPassword = (EditText) findViewById(R.id.widget_edit_text_repeat_password_register);
+        mRegister = (Button) findViewById(R.id.widget_button_register);
+
+        mRegister.setOnClickListener(mOnClickListener);
     }
 
     private boolean validate()
     {
         boolean valid = true;
+        name = mName.getText().toString();
         email = mEmail.getText().toString();
         password = mPassword.getText().toString();
+        userName = mUserName.getText().toString();
+        String repeatPassword = mRepeatPassword.getText().toString();
         if (email.equals(""))
         {
             valid = false;
@@ -74,13 +90,33 @@ public class LoginActivity extends AppCompatActivity
             valid = false;
             mPassword.setError("Required or too short");
         }
-
+        if (repeatPassword.equals(""))
+        {
+            valid = false;
+            mRepeatPassword.setError("Required");
+        }
+        if (!password.equals(repeatPassword))
+        {
+            valid = false;
+            mRepeatPassword.setError("Passwords does not match");
+            mPassword.setError("Passwords does not match");
+        }
+        if (userName.equals(""))
+        {
+            valid = false;
+            mUserName.setError("Required");
+        }
+        if (name.equals(""))
+        {
+            valid = false;
+            mName.setError("Required");
+        }
         return valid;
     }
 
     private void makeRequest() throws JSONException
     {
-        ReqUtils.jsonRequestWithParams(getContext(), Request.Method.POST, ReqConstants.LOGIN, params(), new Response.Listener<JSONObject>()
+        ReqUtils.jsonRequestWithParams(getContext(), Request.Method.POST, ReqConstants.REGISTER, params(), new Response.Listener<JSONObject>()
         {
             @Override
             public void onResponse(JSONObject response)
@@ -89,13 +125,7 @@ public class LoginActivity extends AppCompatActivity
                 boolean success = response.optBoolean("success");
                 if (success)
                 {
-                    Preferences.setToken(getContext().getBaseContext(), response.optString("token"));
-                    Utils.mUser = new User(response.optJSONObject("user"));
 
-                    //TODO: move to MainActivity
-                    Intent intent = new Intent(getContext(), MapsActivity.class);
-                    startActivity(intent);
-                    getContext().finish();
                 }
             }
         }, new Response.ErrorListener()
@@ -111,13 +141,14 @@ public class LoginActivity extends AppCompatActivity
     private JSONObject params() throws JSONException
     {
         JSONObject object = new JSONObject();
-        object.put("username", email);
+        object.put("name", name);
+        object.put("username", userName);
         object.put("password", password);
-
+        object.put("email", email);
         return object;
     }
 
-    //-----------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
 
     private View.OnClickListener mOnClickListener = new View.OnClickListener()
     {
@@ -125,12 +156,12 @@ public class LoginActivity extends AppCompatActivity
         public void onClick(View v)
         {
             int id = v.getId();
+
             switch (id)
             {
-                case R.id.widget_button_login:
+                case R.id.widget_button_register:
                     if (validate())
                     {
-                        //Make request
                         try
                         {
                             makeRequest();
@@ -140,11 +171,6 @@ public class LoginActivity extends AppCompatActivity
                             e.printStackTrace();
                         }
                     }
-                    break;
-                case R.id.widget_text_view_no_account_login:
-                    Intent intent = new Intent(getContext(), RegisterActivity.class);
-                    startActivity(intent);
-                    getContext().finish();
                     break;
             }
         }
---
+++ app/src/main/java/com/fluskat/firefightingeagle/Utils.java
@@ -0,0 +1,12 @@
+package com.fluskat.firefightingeagle;
+
+/**
+ * Created by Erenis Ramadani on 30-Apr-17.
+ */
+
+public class Utils
+{
+
+    public static User mUser;
+
+}
